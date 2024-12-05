1010a7e4efe794cd1fecdd55618df58f0916af62
rajbhuva07 <rajbhuva.cloudus24@gmail.com>
2024-12-05 12:34:57 +0530

    some changes

--- route/pages/index.js
+++ route/pages/index.js
@@ -2,18 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 
-router.get('/', (req, res) => {
-    res.render('pages/index');
-});
-router.get('/blog', (req, res) => {
-    res.render('pages/blog');
-});
-router.get('/class', (req, res) => {
-    res.render('pages/class');
-});
-router.get('/about', (req, res) => {
-    res.render('pages/about');
-});
+// router.get('/', (req, res) => {
+//     res.render('pages/index');
+// });
+// router.get('/blog', (req, res) => {
+//     res.render('pages/blog');
+// });
+// router.get('/class', (req, res) => {
+//     res.render('pages/class');
+// });
+// router.get('/about', (req, res) => {
+//     res.render('pages/about');
+// });
 
 
 
